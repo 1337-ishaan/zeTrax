6899661ee317231593de63fb068bbdf0fad65730
Ishaan Parmar <64636222+1337-ishaan@users.noreply.github.com>
2024-04-23 21:16:31 +0530

    chore: add copy icon

--- packages/site/src/components/utils/Copyable.tsx
+++ packages/site/src/components/utils/Copyable.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components/macro';
+import { ReactComponent as CopyIcon } from '../../assets/copy.svg';
 
 const CopyableWrapper = styled.div`
   background: rgba(0, 0, 0, 0.2);
@@ -9,6 +10,12 @@ const CopyableWrapper = styled.div`
   margin-top: 4px;
   width: fit-content;
   color: #fff;
+  display: flex;
+  align-items: center;
+  .copy-icon {
+    width: 16px;
+    height: 16px;
+  }
 `;
 
 interface CopyableProps {
@@ -21,7 +28,11 @@ const Copyable = ({ children }: CopyableProps): JSX.Element => {
     window.alert('Copied');
   };
 
-  return <CopyableWrapper onClick={copy}>{children}</CopyableWrapper>;
+  return (
+    <CopyableWrapper onClick={copy}>
+      {children} &nbsp; <CopyIcon className="copy-icon" />
+    </CopyableWrapper>
+  );
 };
 
 export default Copyable;
