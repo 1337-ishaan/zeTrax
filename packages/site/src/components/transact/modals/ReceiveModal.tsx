eccafb1e1b80b81f8356588c454fe837adbeb35b
Ishaan Parmar <64636222+1337-ishaan@users.noreply.github.com>
2024-06-01 01:35:00 +0530

    chore: added receive zeta QR

--- packages/site/src/components/transact/modals/ReceiveModal.tsx
+++ packages/site/src/components/transact/modals/ReceiveModal.tsx
@@ -4,14 +4,19 @@ import Send from '../Send';
 import { ReactComponent as CrossIcon } from '../../../assets/cross.svg';
 
 import useAccount from '../../../hooks/useAccount';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { MetaMaskContext } from '../../../hooks';
 import QRCode from 'react-qr-code';
 import Typography from '../../../components/utils/Typography';
 import Copyable from '../../../components/utils/Copyable';
+import FlexColumnWrapper from '../../../components/utils/wrappers/FlexColumnWrapper';
+import FlexRowWrapper from '../../../components/utils/wrappers/FlexWrapper';
 
 const ReceiveModalWrapper = styled.div`
   position: relative;
+  .address-type-wrapper {
+    justify-content: space-around;
+  }
 `;
 
 interface ReceiveModalProps {
@@ -37,7 +42,10 @@ const ReceiveModal = ({
   isReceiveModalOpen,
   setIsReceiveModalOpen,
 }: ReceiveModalProps): JSX.Element => {
-  const { btcAddress } = useAccount(true);
+  const { btcAddress, address } = useAccount(true);
+  const [selectedAddressType, setSelectedAddressType] = useState<'BTC' | 'EVM'>(
+    'BTC',
+  );
   return (
     <ReceiveModalWrapper>
       {btcAddress && (
@@ -53,8 +61,34 @@ const ReceiveModal = ({
               background: '#141417',
               padding: '16px',
               width: 'fit-content',
+              transition: '.5s all',
             }}
           >
+            <FlexRowWrapper
+              style={{ justifyContent: 'space-evenly', alignItems: 'center' }}
+              className="address-type-wrapper"
+            >
+              <FlexColumnWrapper onClick={() => setSelectedAddressType('EVM')}>
+                <Typography
+                  color={selectedAddressType === 'EVM' ? '#fff' : '#a49f9f'}
+                  size={selectedAddressType === 'EVM' ? 24 : 22}
+                >
+                  EVM
+                </Typography>
+              </FlexColumnWrapper>
+              <div
+                style={{ height: '24px', width: '.1px', background: '#fff' }}
+              />
+              <FlexColumnWrapper onClick={() => setSelectedAddressType('BTC')}>
+                <Typography
+                  color={selectedAddressType === 'BTC' ? '#fff' : '#a49f9f'}
+                  size={selectedAddressType === 'BTC' ? 24 : 22}
+                >
+                  BTC
+                </Typography>{' '}
+              </FlexColumnWrapper>
+            </FlexRowWrapper>
+
             <CrossIcon
               onClick={() => setIsReceiveModalOpen(false)}
               style={{
@@ -68,10 +102,15 @@ const ReceiveModal = ({
               }}
             />
             <Typography color="#fff" size={24}>
-              Receive BTC
+              Receive {selectedAddressType === 'BTC' ? 'BTC' : 'ZETA'}
             </Typography>
-            <QRCode style={{ border: '4px solid #fff' }} value={btcAddress} />
-            <Copyable>{btcAddress}</Copyable>
+            <QRCode
+              style={{ border: '4px solid #fff' }}
+              value={selectedAddressType === 'BTC' ? btcAddress : address!}
+            />
+            <Copyable>
+              {selectedAddressType === 'BTC' ? btcAddress : address!}
+            </Copyable>
           </div>
         </Modal>
       )}
